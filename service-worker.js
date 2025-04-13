ec53435bb0f9e80642d6427fb7370ed723a5fb72
ukparangath <ukparangath@gmail.com>
2025-04-13 08:36:23 +0530

    Create service-worker.js for app shell caching

---
+++ service-worker.js
@@ -0,0 +1,18 @@
+const CACHE_NAME = "ukp-cache-v1";
+const urlsToCache = ["admin.html", "manifest.json"];
+
+self.addEventListener("install", function (event) {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(function (cache) {
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+self.addEventListener("fetch", function (event) {
+  event.respondWith(
+    caches.match(event.request).then(function (response) {
+      return response || fetch(event.request);
+    })
+  );
+});
